f78e3ec67328469beb28f898ac99141b00c6ea1d
aleksstar1988 <aleksstar@i.ua>
2020-03-01 11:57:15 +0200

    Twentee first commit. Adding search form to dish.

--- client/src/app/storage/storage.component.ts
+++ client/src/app/storage/storage.component.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../services/storage.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {Router,ActivatedRoute} from '@angular/router';
 import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { Directive, HostListener } from '@angular/core';
 
 
 @Component({
@@ -16,16 +17,17 @@ import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 export class StorageComponent implements OnInit {
  
- 
   ngOnInit() {
-    this.validationFunction();
+    console.log('method ngOnInit()');
    this.getIngradients();
-   
+   this.disabledSearch = false;
   }
 
+  disabledSearch:boolean;
   ingradients: Ingradient[] = [];
 
   searchIngradientWord: string;
+  
 
 
   constructor(private storageService: StorageService,
@@ -33,66 +35,72 @@ export class StorageComponent implements OnInit {
 
   
   refresh(): void {
-    //console.log('selectedId ='+this.selectedId);
-    this.validationFunction();
-     this.getIngradients();
+    console.log('method reflesh()');
+    //this.getIngradients();
+    
+   
+  }
+
+  onKey() { // without type info
+    if(this.searchIngradientWord.length > 0)
+    this.disabledSearch = true;
+   console.log('on key is working');
+   console.log('searchIngradientWord = '+this.searchIngradientWord);
+  }
+
+  /*@HostListener('click', ['$event.target.id']) onClick(id: 'searchButton') {
+    alert(`You clicked on button`);
+  } */
+
+validationFunction(): void {
+  console.log("window.addEventListener('load', function()) in the storage ");
+  var forms = document.getElementsByClassName('needs-validation');
+  console.log("forms.item = "+forms.item);
+  console.log("forms.length = "+forms.length);
+  console.log("forms.namedItem = "+forms.namedItem);
+  var validation = Array.prototype.filter.call(forms, function(form) {
+    console.log("why you are not here?");
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        this.disabledSearch = false;
+        console.log("Validation has been working!");
+        console.log("this.disabledSearch"+this.disabledSearch);
+        event.preventDefault();
+        event.stopPropagation();
+       }
+      form.classList.add('was-validated');
+      
+    }, false);
+  });
+}
+
+
+ @HostListener('click', ['$event.target.id']) onClick(id: 'searchButton') {
+     this.validationFunction();
+     console.log("this.searchIngradientWord = "+this.searchIngradientWord);
+     console.log("this.disabledSearch = "+this.disabledSearch);
   }
 
 
 
-  getIngradients(): void {
+  getIngradients(): void {   
     this.storageService.getIngradients()
     .subscribe(ingradients => 
       { 
         this.ingradients = ingradients.body;
-      //  this.returnedBooks = this.books.slice(0, 10);
-      }); 
-
-      
+      });     
   }
-
- 
-  
-
-/*
-  addNewEmployee() :void {
-    this.router.navigateByUrl('/addemployee/add');
-  }
-*/
-
 
   addNewIngradient() :void {
     this.router.navigateByUrl('/addingradient/add');
   }
 
-  validationFunction() {
-    'use strict';
-    window.addEventListener('load', function() {
-      console.log("window.addEventListener('load', function()) in the storage ");
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      console.log("forms.item = "+forms.item);
-      console.log("forms.length = "+forms.length);
-      console.log("forms.namedItem = "+forms.namedItem);
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        console.log("why you are not here?");
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  };
-
-
-  findIngradientByName(): void {
+ 
+ /* findIngradientByName(): void {
     this.validationFunction();
-    console.log("this.searchIngradientWord = "+this.searchIngradientWord);
-};
+     console.log("this.searchIngradientWord = "+this.searchIngradientWord);
+     console.log("this.disabledSearch = "+this.disabledSearch);
+};*/
 
 
 
